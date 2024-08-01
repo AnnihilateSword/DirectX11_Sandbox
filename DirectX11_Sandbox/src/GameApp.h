6c6b5168870483297f09f8c8265df18301c64469
Jinkun <Jinkun@qq.com>
2024-08-01 22:21:01 +0800

    <feat>: DirectX11 Initialization

---
+++ DirectX11_Sandbox/src/GameApp.h
@@ -0,0 +1,13 @@
+#include "d3dApp.h"
+
+class GameApp : public D3DApp
+{
+public:
+    GameApp(HINSTANCE hInstance, const std::wstring& windowName, int initWidth, int initHeight);
+    ~GameApp();
+
+    bool Init();
+    void OnResize();
+    void UpdateScene(float dt);
+    void DrawScene();
+};
